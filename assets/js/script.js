238147c867ddc2ce2787015a94c294f8c940742f
MaheshS03 <maheshraina375@gmail.com>
2024-02-27 19:19:03 +0530

    Increment and Decrement - JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
-const incrementFunction = () => {
+const incrementFunction = (click) => {
+    const counter = document.getElementById("counter-number")
+    const sum = parseInt(counter.innerText) + click;
+    console.log(sum+click)
+    counter.innerText = sum;
     console.log(`increment logged!`)
 }
 
-const decrementFunction = () => {
+const decrementFunction = (click) => {
+    const counter = document.getElementById("counter-number")
+    const sum = parseInt(counter.innerText) + click;
+    console.log(sum-click)
+    counter.innerText = sum;
     console.log(`decrement logged!`)
 }
